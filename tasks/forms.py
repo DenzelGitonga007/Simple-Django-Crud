ec4f8c5ac5960473fd4668b65ef3cde726f0b635
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-10-03 09:42:14 +0300

    task views

--- tasks/forms.py
+++ tasks/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Task
 
 # Form to create the task
-class TaskCreate(forms.ModelForm):
+class TaskCreateForm(forms.ModelForm):
     """ Form to create a task """
     model = Task
     fields = ['title', 'description', 'status']
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . models import Task
+from . forms import TaskCreateForm
 
 # Create your views here.
+# Create Task
+def create_task(request):
+    """ Create task """
+    if request.method == 'POST':
+        form = TaskCreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskCreateForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'tasks/create_task.html', context)
+
+# Read all tasks
+def task_list(request):
+    """ View all tasks """
+    tasks = Task.objects.all()
+    context = {
+        'tasks': tasks
+    }
+    return render(request, 'tasks/task_list.html', context)
+
+# Edit task
+def update_task(request, pk):
+    """ Update/edit task """
+    task = Task.objects.get(id=pk)
+    if request.method == 'POST':
+        form = TaskCreateForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskCreateForm(instance=task)
+    context = {
+        'form': form
+    }
+    return render(request, 'tasks/update_task.html', context)
+
+# Delete task
+def delete_task(request, pk):
+    """ Delete task """
+    task = Task.objects.get(id=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    context = {
+        'task': task
+    }
+    return render(request, 'tasks/confirm_delete.html', context)
+
